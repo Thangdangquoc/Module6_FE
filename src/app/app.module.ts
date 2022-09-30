28cec14aa33937ba5f7aa7b64d6785215305cdf3
DTV <dthang277@gmail.com>
2022-09-30 15:17:55 +0700

    lên

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './share/footer/footer.component';
+import { NavbarComponent } from './share/navbar/navbar.component';
+import { LoginComponent } from './sercurity/login/login.component';
+import { CustomerRegisterComponent } from './sercurity/customer-register/customer-register.component';
+import { MerchantRegisterComponent } from './sercurity/merchant-register/merchant-register.component';
+import { HomeComponent } from './showhome/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    LoginComponent,
+    CustomerRegisterComponent,
+    MerchantRegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
